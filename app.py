ebdadb88b5f7b9c7e267645abc109820b9b53a05
ReubenAdeniyi <adedapoadeniyi@gmail.com>
2022-09-02 19:53:47 -0500

    added docker updating step

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import logging
 
 import pandas as pd
@@ -27,32 +26,32 @@ def home():
     return html.format(format)
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     """Performs an sklearn prediction
-        input looks like:
-        {
-        "CHAS":{
-        "0":0
-        },
-        "RM":{
-        "0":6.575
-        },
-        "TAX":{
-        "0":296.0
-        },
-        "PTRATIO":{
-        "0":15.3
-        },
-        "B":{
-        "0":396.9
-        },
-        "LSTAT":{
-        "0":4.98
-        }
-        result looks like:
-        { "prediction": [ <val> ] }
-        """
+    input looks like:
+    {
+    "CHAS":{
+    "0":0
+    },
+    "RM":{
+    "0":6.575
+    },
+    "TAX":{
+    "0":296.0
+    },
+    "PTRATIO":{
+    "0":15.3
+    },
+    "B":{
+    "0":396.9
+    },
+    "LSTAT":{
+    "0":4.98
+    }
+    result looks like:
+    { "prediction": [ <val> ] }
+    """
 
     # Logging the input payload
     json_payload = request.json
@@ -65,10 +64,10 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     LOG.info(f"Output prediction value: {prediction}")
-    return jsonify({'prediction': prediction})
+    return jsonify({"prediction": prediction})
 
 
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=8000, debug=True) 
+    app.run(host="0.0.0.0", port=8000, debug=True)
